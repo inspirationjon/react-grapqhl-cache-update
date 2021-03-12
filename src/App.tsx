cd3c4900342daa60f849d5af15a874f48c7f4508
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-03-12 09:06:02 +0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useMutation, useQuery } from '@apollo/client'
 
+const READ_BOOKS = gql`
+    query readBooks {
+        books {
+            name
+            id
+        }
+    }
+`
+
+const WRITE_QUERY = gql`
+    mutation createBook($name: String!) {
+        createBook(name: $name) {
+            name
+        }
+    }
+`
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, error, data } = useQuery(READ_BOOKS)
+
+    const [
+        createBook,
+        { loading: creating, error: creatingError },
+    ] = useMutation(WRITE_QUERY, {
+        update: (cache, { data: { createBook } }) => {
+            cache.modify({
+                fields: {
+                    books(books = []) {
+                        return [createBook, ...books]
+                    },
+                },
+            })
+        },
+    })
+
+    return (
+        <>
+            {loading && <h1> Loading...</h1>}
+            {error && <h1> Error {error.message}</h1>}
+
+            {creating && <>creating...</>}
+            {creatingError && <>{creatingError.message}</>}
+
+            <input
+                type='text'
+                autoComplete='off'
+                spellCheck={false}
+                autoFocus={true}
+                onKeyUp={(evt): void => {
+                    if (
+                        evt.key === 'Enter' &&
+                        evt.currentTarget.value.length > 0
+                    ) {
+                        createBook({
+                            variables: { name: evt.currentTarget.value.trim() },
+                        })
+
+                        evt.currentTarget.value = ''
+                    }
+                }}
+                placeholder='Todo ...'
+            />
+
+            {data && (
+                <ul>
+                    {data.books.map((book: any) => (
+                        <li key={book.id}>{book.name}</li>
+                    ))}
+                </ul>
+            )}
+        </>
+    )
 }
 
-export default App;
+export default App
